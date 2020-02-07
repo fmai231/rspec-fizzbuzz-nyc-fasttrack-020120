55ad590bf878e3908fccef7930cf0e81bd563646
Franky Mai <frankymai91@gmail.com>
2020-02-07 21:43:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,18 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  if    num % 3 == 0 and num % 5 == 0
+        puts 'FizzBuzz'
+  elsif num % 3 == 0
+        puts 'fizz'
+  elsif num % 5 == 0
+        puts 'buzz'
+  else
+        puts"Nil"
+end
+end
+
+
+fizzbuzz(3) = fizz_3
+fizzbuzz(5) = fizz_5
+fizzbuzz(15)
+fizzbuzz(16)
